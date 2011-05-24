499ab9995fdd5581f931a3532faa95d5735e4e64
Alexandre Angelim <angelim@angelim.com.br>
2011-05-24 14:25:59 -0300

    translating benchmarks

--- lib/performance/performance.rb
+++ lib/performance/performance.rb
@@ -1,5 +1,6 @@
 require "benchmark"
 require "mongoid"
+require "mongoid-mirrored"
 
 def rand_post(posts)
   posts[rand(posts.size)]
@@ -17,7 +18,6 @@ class Post
   include Mongoid::Timestamps
   
   field :title
-  # embeds_many :comments, :class_name => "Post::Comment"
   has_many :comments
   
 end
@@ -26,136 +26,148 @@ class Comment
   include Mongoid::Document
   include Mongoid::Timestamps
   field :contents
-  # include Mongoid::Mirrored
   
-  # mirrored_in :post, :belongs => true do
-  #   field :contents
-  #   field :counter, :type => Integer
-  # end
   belongs_to :post, :index => true
 end
 
+puts "----------------------------------------------------"
+puts "Benchmark for referenced documents"
+puts "----------------------------------------------------"
+
 Benchmark.bm do |bm|
-  10000.times do 
+  500.times do 
     Post.create
   end
   
   posts = Post.all.to_a
   
-	bm.report "criando comments da raiz" do
-		10000.times do
-			Comment.create(:post => rand_post(posts))
-		end
-	end
-	
-	bm.report "atualizando comments da raiz" do
-		Comment.all.each_with_index do |c,i|
-			c.update_attribute(:contents, i)
-		end
-	end
-	
-	
-	bm.report "leitura de var no relacionamento" do
-	  posts.each do |p|
-  	  p.comments.each do |c|
-  	    (con ||= []) << c.contents
+  bm.report "creating comments from root collection" do
+    10000.times do
+      Comment.create(:post => rand_post(posts))
+    end
+  end
+  
+  bm.report "updating comments from root collection" do
+    Comment.all.each_with_index do |c,i|
+      c.update_attribute(:contents, i)
+    end
+  end
+  
+  bm.report "traversing posts with comments" do
+    10000.times do 
+      p = rand_post(posts)
+      p.comments.each do |c|
+        (con ||= []) << c.contents
       end
     end
   end
-	
-	bm.report "apagando comments da raiz" do
-		Comment.all.map(&:destroy)
-	end
-	
-	bm.report "criando comments do embedded" do
-		10000.times do
-		  p = rand_post(posts)
-			p.comments.create
-		end
-	end
-	
-	bm.report "atualizando comments da embedded" do
+  
+  bm.report "deleting comments from root collection" do
+    Comment.all.map(&:destroy)
+  end
+  
+  bm.report "creating comments from embedding collection" do
+    10000.times do
+      p = rand_post(posts)
+      p.comments.create
+    end
+  end
+  
+  bm.report "updating comments from embedding collection" do
     posts.each do |p|
-  		p.comments.each_with_index do |c,i|
-  			c.update_attribute(:contents, i)
-  		end
-  	end
-	end
-	
-	bm.report "apagando comments da embedded" do
-	  posts.each do |p|
-  		p.comments.map(&:destroy)
-  	end
-	end	
+      p.comments.each_with_index do |c,i|
+        c.update_attribute(:contents, i)
+      end
+    end
+  end
+  
+  bm.report "deleting comments from embedding collection" do
+    posts.each do |p|
+      p.comments.map(&:destroy)
+    end
+  end
 end
 
 Mongoid.master.collections.select {|c| c.name !~ /system/ }.each(&:drop)
 
+Object.send :remove_const, "Post"
 class Post
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  
+  field :title
   embeds_many :comments, :class_name => "Post::Comment"
-  # Post.collection.create_index "comments._id"
 end
 
+Object.send :remove_const, "Comment"
 class Comment
+  include Mongoid::Document
+  include Mongoid::Timestamps
   include Mongoid::Mirrored
   
-  mirrored_in :post, :belongs => true do
+  field :contents
+  
+  belongs_to :post
+  
+  mirrored_in :post, :index => true do
     field :contents
     field :counter, :type => Integer
   end
 end
 
-
+puts "----------------------------------------------------"
+puts "Benchmark for mirrored documents"
+puts "----------------------------------------------------"
 Benchmark.bm do |bm|
-  10000.times do 
+  500.times do 
     Post.create
   end
   
   posts = Post.all.to_a
   
-	bm.report "criando comments da raiz" do
-		10000.times do
-			Comment.create(:post => rand_post(posts))
-		end
-	end
-	
-	bm.report "atualizando comments da raiz" do
-		Comment.all.each_with_index do |c,i|
-			c.update_attribute(:contents, i)
-		end
-	end
-	
-	
-	bm.report "leitura de var no relacionamento" do
-	  posts.each do |p|
-  	  p.comments.each do |c|
-  	    (con ||= []) << c.contents
+  bm.report "creating comments from root collection" do
+    10000.times do
+      Comment.create(:post => rand_post(posts))
+    end
+  end
+  
+  bm.report "updating comments from root collection" do
+    Comment.all.each_with_index do |c,i|
+      c.update_attribute(:contents, i)
+    end
+  end
+  
+  bm.report "traversing posts with comments" do
+    10000.times do 
+      p = rand_post(posts)
+      p.comments.each do |c|
+        (con ||= []) << c.contents
       end
     end
   end
-	
-	bm.report "apagando comments da raiz" do
-		Comment.all.map(&:destroy)
-	end
-	
-	bm.report "criando comments do embedded" do
-		10000.times do
-		  p = rand_post(posts)
-			p.comments.create
-		end
-	end
-	
-	bm.report "atualizando comments da embedded" do
+  
+  bm.report "deleting comments from root collection" do
+    Comment.all.map(&:destroy)
+  end
+  
+  bm.report "creating comments from embedding collection" do
+    10000.times do
+      p = rand_post(posts)
+      p.comments.create
+    end
+  end
+  
+  bm.report "updating comments from embedding collection" do
     posts.each do |p|
-  		p.comments.each_with_index do |c,i|
-  			c.update_attribute(:contents, i)
-  		end
-  	end
-	end
-	
-	bm.report "apagando comments da embedded" do
-	  posts.each do |p|
-  		p.comments.map(&:destroy)
-  	end
-	end	
+      p.comments.each_with_index do |c,i|
+        c.update_attribute(:contents, i)
+      end
+    end
+  end
+  
+  bm.report "deleting comments from embedding collection" do
+    posts.each do |p|
+      p.comments.map(&:destroy)
+    end
+  end
 end
