7ad26aab474a9462d4318e2ca0ad12fec43b0c05
Alexandre Angelim <angelim@angelim.com.br>
2011-05-24 05:29:21 -0300

    implementing siblings replication

--- lib/mongoid-mirrored/base.rb
+++ lib/mongoid-mirrored/base.rb
@@ -26,46 +26,151 @@ module Mongoid
         self.embedding_options[:background_index] = false if self.embedding_options[:background_index].nil?
       end
       
+      def define_after_create_callback(_embedding_model, mirror_klass)
+        unless mirror_klass.instance_methods.include?(:_create_root)
+          mirror_klass.class_eval <<-EOF
+            after_create  :_create_root
+          EOF
+        end
+
+        mirror_klass.class_eval <<-EOF
+          def _create_root
+            #{@root_klass}.collection.insert(attributes.merge(:#{_embedding_model}_id => #{_embedding_model}.id))
+          end
+        EOF
+      end
+      def define_after_update_callback(_embedding_model, mirror_klass)
+        unless mirror_klass.instance_methods.include?(:_update_root)
+          mirror_klass.class_eval <<-EOF
+            after_update  :_update_root
+          EOF
+        end
+
+        mirror_klass.class_eval <<-EOF
+           def _update_root
+             #{@root_klass}.collection.update({ :_id => id }, '$set' => attributes.except('_id'))
+           end
+         EOF
+      end
+      
+      def define_after_destroy_callback(_embedding_model, mirror_klass)
+        unless mirror_klass.instance_methods.include?(:_destroy_root)
+          mirror_klass.class_eval <<-EOF
+            after_destroy  :_destroy_root
+          EOF
+        end
+
+        mirror_klass.class_eval <<-EOF
+           def _destroy_root
+             #{@root_klass}.collection.update({ :_id => id }, '$set' => attributes.except('_id'))
+           end
+         EOF
+      end
+      
+      def define_after_create_siblings(_embedding_model, mirror_klass)
+        unless mirror_klass.instance_methods.include?(:_create_siblings)
+          mirror_klass.class_eval <<-EOF
+            after_create  :_create_siblings
+          EOF
+        end
+        mirror_klass.class_eval <<-EOF
+          def _create_siblings
+            #{embedding_models}.each do |sibling|
+              _sibling = sibling.to_s
+              next if sibling == :#{_embedding_model}
+              sibling_klass = sibling.to_s.classify.constantize
+              if self[_sibling+"_id"]
+                sibling_klass.collection.update({ :_id => self[_sibling+"_id"] }, '$push' => {:#{root_association} => attributes.merge(:#{_embedding_model}_id => #{_embedding_model}.id)})
+              end
+            end
+          end
+        EOF
+      end
+      
+      def define_after_update_siblings(_embedding_model, mirror_klass)
+        unless mirror_klass.instance_methods.include?(:_update_siblings)
+          mirror_klass.class_eval <<-EOF
+            after_update  :_update_siblings
+          EOF
+        end
+        mirror_klass.class_eval <<-EOF
+          def _update_siblings
+            #{embedding_models}.each do |sibling|
+              _sibling = sibling.to_s
+              next if sibling == :#{_embedding_model}
+              sibling_klass = sibling.to_s.classify.constantize
+              if self[_sibling+"_id"]
+                nested_attr = {}
+                attributes.except("#{_embedding_model}_id").each_pair do |k,v|
+                  nested_attr["#{root_association}.$."+k] = v
+                end
+                sibling_klass.collection.update({"#{root_association}._id" => id}, '$set' => nested_attr )
+              end
+            end
+          end
+        EOF
+      end
+      
+      def define_after_destroy_siblings(_embedding_model, mirror_klass)
+        unless mirror_klass.instance_methods.include?(:_destroy_siblings)
+          mirror_klass.class_eval <<-EOF
+            after_destroy  :_destroy_siblings
+          EOF
+        end
+        mirror_klass.class_eval <<-EOF
+          def _destroy_siblings
+            #{embedding_models}.each do |sibling|
+              _sibling = sibling.to_s
+              next if sibling == :#{_embedding_model}
+              sibling_klass = sibling.to_s.classify.constantize
+              if self[_sibling+"_id"]
+                sibling_klass.collection.update({ :_id => self[_sibling+"_id"] }, '$pull' => {:#{root_association} => { :_id => id}})
+              end
+            end
+          end
+        EOF
+      end
+      
       # Define callbacks for mirror class that don't trigger callbacks on the root class
       def define_mirror_callbacks_for(_embedding_model, mirror_klass)
         if [:both, :from_mirror].include?(embedding_options[:sync_direction])
           if embedding_options[:sync_events].include?(:create) || embedding_options[:sync_events] == [:all]
-            mirror_klass.class_eval <<-EOF
-              after_create  :_create_root
-              def _create_root
-                #{@root_klass}.collection.insert(attributes.merge(:#{_embedding_model}_id => #{_embedding_model}.id))
-              end
-            EOF
+            define_after_create_callback(_embedding_model, mirror_klass)
+            if embedding_options[:replicate_to_siblings] && embedding_models.size > 1
+              define_after_create_siblings(_embedding_model, mirror_klass)
+            end
           end
           
           if embedding_options[:sync_events].include?(:update) || embedding_options[:sync_events] == [:all]
-            mirror_klass.class_eval <<-EOF
-              after_update  :_update_root
-              def _update_root
-                #{@root_klass}.collection.update({ :_id => id }, '$set' => attributes.except('_id'))
-              end
-            EOF
+            define_after_update_callback(_embedding_model, mirror_klass)
+            if embedding_options[:replicate_to_siblings] && embedding_models.size > 1
+              define_after_update_siblings(_embedding_model, mirror_klass)
+            end
           end
           
           if embedding_options[:sync_events].include?(:destroy) || embedding_options[:sync_events] == [:all]
-            mirror_klass.class_eval <<-EOF
-              after_destroy  :_destroy_root
-              def _destroy_root
-                #{@root_klass}.collection.remove({ :_id => id })
-              end
-            EOF
+            define_after_destroy_callback(_embedding_model, mirror_klass)
+            if embedding_options[:replicate_to_siblings] && embedding_models.size > 1
+              define_after_destroy_siblings(_embedding_model, mirror_klass)
+            end
           end
         end
+      end
+      
+      # name of the association used by the embedding class
+      # eg: comments
+      def root_association
+        inverse_of = @root_klass.name.underscore
+        inverse_of = inverse_of.pluralize if embedding_options[:inverse_of] == :many
+        inverse_of
       end
       
       def embeds_mirror_in(_embedding_model, mirror_klass)
         # mongoid macro embedded_in
         # eg: embedded_in :post, :inverse_of => :comments
-        inverse_of = @root_klass.name.underscore
-        inverse_of = inverse_of.pluralize if embedding_options[:inverse_of] == :many
         
         mirror_klass.class_eval <<-EOT
-          embedded_in :#{_embedding_model}, :inverse_of => :#{inverse_of}
+          embedded_in :#{_embedding_model}, :inverse_of => :#{root_association}
         EOT
       end
       
@@ -115,11 +220,6 @@ module Mongoid
         end
         yield
         
-        root_klass = self
-        # name of the association used by the embedding class
-        # eg: comments
-        root_association = root_klass.name.downcase.pluralize
-        
         # Defining callbacks for the root document
         define_method :create_mirror do
           
@@ -133,8 +233,7 @@ module Mongoid
             
             # Only tries to create mirrored document if the embedding instance is given
             if embedding_instance
-              # eg: Post.comments.create(attributes.merge!(:skip_callbacks => true).except(:post_id))
-              _embedding_klass.collection.update({ :_id => embedding_instance.id }, '$push' => {root_association => attributes.except("#{embedding_string}_id")})
+              _embedding_klass.collection.update({ :_id => embedding_instance.id }, '$push' => {self.class.root_association => attributes.except("#{embedding_string}_id")})
             end
           end
         end
@@ -146,7 +245,6 @@ module Mongoid
             
             # attributes that will be used to define the root and embedding classes and instances
             embedding_string = embedding_model.to_s
-            # embedding_klass = embedding_string.classify.constantize
             embedding_instance = eval(embedding_string)
             _embedding_klass = self.class.embedding_klass(embedding_model)
             
@@ -160,9 +258,9 @@ module Mongoid
                 # eg: contents => ;comments.$.contents =>
                 nested_attr = {}
                 attributes.except("#{embedding_string}_id").each_pair do |k,v|
-                  nested_attr["#{root_association}.$.#{k}"] = v
+                  nested_attr["#{self.class.root_association}.$.#{k}"] = v
                 end
-                _embedding_klass.collection.update({"#{root_association}._id" => id}, '$set' => nested_attr )
+                _embedding_klass.collection.update({"#{self.class.root_association}._id" => id}, '$set' => nested_attr )
               end
             end
           end
@@ -174,12 +272,11 @@ module Mongoid
             
             # attributes that will be used to define the root and embedding classes and instances
             embedding_string = embedding_model.to_s
-            # embedding_klass = embedding_string.classify.constantize
             _embedding_klass = self.class.embedding_klass(embedding_model)
             embedding_instance = eval(embedding_string)
             if embedding_instance
               id_to_destroy = changed_embedding_instance || embedding_instance.id
-              _embedding_klass.collection.update({ :_id => id_to_destroy }, '$pull' => {root_association => { :_id => id}})
+              _embedding_klass.collection.update({ :_id => id_to_destroy }, '$pull' => {self.class.root_association => { :_id => id}})
             end
           end
         end
--- spec/functional/mongoid/comment_spec.rb
+++ spec/functional/mongoid/comment_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "Comment" do
   let(:article) { Article.create }
-  
+  let(:user) { User.create }
   context "creating from the root collection" do
     it "should create document in root collection" do
       c = Comment.create(:article => article, :contents => "root", :author => "root_author" )
@@ -10,10 +10,13 @@ describe "Comment" do
     end
     
     it "should create similar document in each embedding collection" do
-      c  = Comment.create(:article => article, :contents => "root", :author => "root_author" )
+      c  = Comment.create(:article => article, :user => user, :contents => "root", :author => "root_author" )
       article.reload
       article.comments.find(c.id.to_s).should_not be_nil
+      user.reload
+      user.comments.find(c.id.to_s).should_not be_nil
     end
+    
   end
   
   context "creating from the embedding collection" do
@@ -29,7 +32,9 @@ describe "Comment" do
     end
     
     it "should replicate document to sibling collections" do
-      pending
+      c = article.comments.create(:user_id => user.id, :contents => "embedding", :author => "embedding_author")
+      user.reload
+      user.comments.find(c.id).should_not be_nil
     end
   end
 
@@ -42,17 +47,21 @@ describe "Comment" do
     end
     
     it "should update document in embedding collection with new attributes" do
-      c  = Comment.create(:article => article, :contents => "root", :author => "root_author" )
+      c  = Comment.create(:article => article, :user => user, :contents => "root", :author => "root_author" )
       c.update_attributes(:contents => "new_root", :author => "new_author_root")
       article.reload
       article.comments.where(:contents => "new_root", :author => "new_author_root").first.id.should == c.id
+      user.reload
+      user.comments.where(:contents => "new_root", :author => "new_author_root").first.id.should == c.id
     end
     context "switching embedding document" do
       it "should delete comment from embedding original collection" do
         c = article.comments.create(:contents => "embedding", :author => "embedding_author")
-        Comment.first.update_attribute(:article_id, Article.create.id)
+        Comment.first.update_attributes(:article_id =>  Article.create.id, :user_id => User.create.id)
         article.reload
         article.comments.should be_empty
+        user.reload
+        user.comments.should be_empty
       end
       it "should create comment in new embedding collection" do
         c = article.comments.create(:contents => "embedding", :author => "embedding_author")
@@ -67,7 +76,6 @@ describe "Comment" do
       c = article.comments.create(:contents => "embedding", :author => "embedding_author")
       c.update_attributes(:contents => "new_embedding", :author => "new_author_embedding")
       Comment.where(:contents => "new_embedding", :author => "new_author_embedding").first.id.should == c.id
-      
     end
     
     it "should update similar document in embedding collection" do
@@ -78,22 +86,27 @@ describe "Comment" do
     end
     
     it "should replicate changes to sibling collections" do
-      pending
+      c = article.comments.create(:user_id => user.id, :contents => "embedding", :author => "embedding_author")
+      c.update_attributes(:contents => "new_embedding", :author => "new_author_embedding")
+      user.reload
+      user.comments.find(c.id).contents.should == "new_embedding"
     end
   end
   
   context "destroying from the root collection" do
     it "should destroy document in root collection" do
-      c  = Comment.create(:article => article, :contents => "root", :author => "root_author" )
+      c  = Comment.create(:article => article, :user => user, :contents => "root", :author => "root_author" )
       c.destroy
       Comment.where(:content => "root").should be_empty
     end
     
     it "should destroy similar document in each embedding collection" do
-      c  = Comment.create(:article => article, :contents => "root", :author => "root_author" )
+      c  = Comment.create(:article => article, :user => user, :contents => "root", :author => "root_author" )
       c.destroy
       article.reload
       article.comments.where(:contents => "root").should be_empty
+      user.reload
+      user.comments.where(:contents => "root").should be_empty
     end
   end
   
@@ -112,7 +125,10 @@ describe "Comment" do
     end
     
     it "should destroy document from sibling collections" do
-      pending
+      c = article.comments.create(:user_id => user.id, :contents => "embedding", :author => "embedding_author")
+      c.destroy
+      user.reload
+      user.comments.where(:_id => c.id).should be_empty
     end
   end
   
